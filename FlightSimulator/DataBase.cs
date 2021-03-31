863685a7db7f4f069069b3cea7c4d46401dbbccc
noamiel <51541904+noamiel@users.noreply.github.com>
2021-03-31 21:23:38 +0300

    noa's commit

---
+++ FlightSimulator/DataBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlightSimulator2
+{
+    class DataBase
+    {
+        private string[][] data;
+
+        public int numberOfRows
+        {
+            get;
+        }
+
+        public DataBase(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            var rows = new List<string[]>();
+            numberOfRows = 0;
+
+            while (!reader.EndOfStream)
+            {
+                string[] row = reader.ReadLine().Split(',');
+                rows.Add(row);
+                numberOfRows++;
+            }
+
+            this.data = rows.ToArray();
+        }
+
+        public string getRowAtIndex(int index)
+        {
+            return String.Join(",", data[index]);
+        }
+
+        public float[] getColumnAtIndex(int index)
+        {
+            float[] column = new float[numberOfRows];
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                column[i] = float.Parse(data[i][index]);
+            }
+            return column;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/omerz/Desktop/אוניברסיטה/FG/src/reg_flight.csv";
+            // this is the path on my computer. i dont know how to define a path that will be accurate on every computer
+            DataBase dataBase = new DataBase(path);
+
+            var client = new TcpClient("localhost", 5400);
+            var stream = client.GetStream();
+
+            int currentRowIndex = 0; // bind to "currentRow" to jump to another frame
+            int lastRowIndex = dataBase.numberOfRows;
+            int timeToSleep = 100; // bind to "timeToSleep" to change the speed
+
+            while (currentRowIndex != lastRowIndex)
+            {
+                var row = Encoding.ASCII.GetBytes(dataBase.getRowAtIndex(currentRowIndex) + "\n");
+                stream.Write(row, 0, row.Length);
+                Console.Write(dataBase.getRowAtIndex(currentRowIndex));
+                Thread.Sleep(timeToSleep);
+                currentRowIndex++;
+            }
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ FlightSimulator/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulator2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            this.Title = "Example 2";
+            this.Points = new List<DataPoint>
+                              {
+                                  new DataPoint(0, 4),
+                                  new DataPoint(10, 13),
+                                  new DataPoint(20, 15),
+                                  new DataPoint(30, 16),
+                                  new DataPoint(40, 12),
+                                  new DataPoint(50, 12)
+                              };
+        }
+
+        public string Title { get; private set; }
+
+        public IList<DataPoint> Points { get; private set; }
+    }
+}
